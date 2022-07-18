a2d0e8bdee2402de6b2b3b6f1204e138c6890712
xiaocheng <xiaoch2010@gmail.com>
2022-07-18 13:57:49 +0800

    rename

--- helix-contract/contracts/mapping-token/test/MockSub2SubMessageHandle.sol
+++ helix-contract/contracts/mapping-token/test/MockSub2SubMessageHandle.sol
@@ -5,13 +5,13 @@ import "../v2/AccessController.sol";
 
 contract MockSub2SubMessageHandle is AccessController {
     address remoteHelix;
-    mapping(bytes4=>uint64) outboundNonces;
-    mapping(bytes4=>uint64) inboundNonces;
+    mapping(bytes4=>uint64) outboundMessages;
+    mapping(bytes4=>uint64) inboundMessages;
     bytes4  public inboundLaneId;
     bytes4  public outboundLaneId;
     constructor(uint64 _outboundNonce, uint64 _inboundNonce) {
-        inboundNonces[inboundLaneId] = _inboundNonce;
-        outboundNonces[outboundLaneId] = _outboundNonce;
+        inboundMessages[inboundLaneId] = _inboundNonce;
+        outboundMessages[outboundLaneId] = _outboundNonce;
         _initialize(msg.sender);
     }
 
@@ -44,8 +44,8 @@ contract MockSub2SubMessageHandle is AccessController {
             receiver,
             message
         );
-        uint64 nonce = outboundNonces[outboundLaneId] + 1;
-        outboundNonces[outboundLaneId] = nonce;
+        uint64 nonce = outboundMessages[outboundLaneId] + 1;
+        outboundMessages[outboundLaneId] = nonce;
         remoteHelix.call(recv);
         return encodeMessageId(outboundLaneId, nonce);
     }
@@ -54,11 +54,11 @@ contract MockSub2SubMessageHandle is AccessController {
         require(hasRole(CALLER_ROLE, receiver), "MockS2sMessageHandle:receiver is not caller");
         // don't make sure this success to simulate the failed case.
         receiver.call(message);
-        inboundNonces[inboundLaneId] = inboundNonces[inboundLaneId] + 1;
+        inboundMessages[inboundLaneId] = inboundMessages[inboundLaneId] + 1;
     }
 
     function lastRecvMessageId() public view returns(uint256) {
-        return encodeMessageId(inboundLaneId, inboundNonces[inboundLaneId]);
+        return encodeMessageId(inboundLaneId, inboundMessages[inboundLaneId]);
     }
 
     function fee() public view returns(uint256) {
@@ -75,7 +75,7 @@ contract MockSub2SubMessageHandle is AccessController {
 
     function isMessageDelivered(uint256 messageId) public view returns(bool) {
         (bytes4 laneId, uint64 nonce) = decodeMessageId(messageId);
-        return nonce <= inboundNonces[laneId];
+        return nonce <= inboundMessages[laneId];
     }
 }
 

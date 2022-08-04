8412d7bab991eb4dcf620f9b682951f4eb1786f6
xiaocheng <xiaoch2010@gmail.com>
2022-08-04 11:19:14 +0800

    deploy darwinia crab s2s

--- helix-contract/contracts/mapping-token/v2/erc20-mapping-protocol/WETH.sol
+++ helix-contract/contracts/mapping-token/v2/erc20-mapping-protocol/WETH.sol
@@ -3,8 +3,8 @@
 pragma solidity ^0.4.18;
 
 contract WETH9 {
-    string public name     = "Wrapped Ether";
-    string public symbol   = "WETH";
+    string public name     = "Wrapped Ring";
+    string public symbol   = "WRING";
     uint8  public decimals = 18;
 
     event  Approval(address indexed src, address indexed guy, uint wad);

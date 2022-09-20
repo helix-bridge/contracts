a375cab1aa22a33c478c19ed276f8125e14f4156
xiaocheng <xiaoch2010@gmail.com>
2022-09-20 17:33:53 +0800

    fix latest delivery nonce

--- helix-contract/contracts/mapping-token/v2/erc20-mapping-protocol/Erc20Sub2EthBacking.sol
+++ helix-contract/contracts/mapping-token/v2/erc20-mapping-protocol/Erc20Sub2EthBacking.sol
@@ -163,7 +163,7 @@ contract Erc20Sub2EthBacking is Backing, DailyLimit, IBacking {
         uint256 amount
     ) public onlyMessageEndpoint whenNotPaused {
         expendDailyLimit(wToken, amount);
-        uint256 transferId = IHelixSub2EthMessageEndpoint(messageEndpoint).lastDeliveredMessageId() + 1;
+        uint256 transferId = IHelixSub2EthMessageEndpoint(messageEndpoint).lastDeliveredMessageId();
         require(BitMaps.get(unlockedTransferIds, transferId) == false, "Backing:message has been accepted");
         BitMaps.set(unlockedTransferIds, transferId);
         if (guard != address(0)) {

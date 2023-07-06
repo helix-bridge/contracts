f4d2c79c4680a8510dd6cc3eecd80a798388c7c9
xiaoch05 <xiaoch2010@gmail.com>
2023-07-06 13:54:17 +0800

    interface file

---
+++ helix-contract/contracts/ln/interface/ILnOppositeBridgeSource.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.10;
+
+interface ILnOppositeBridgeSource {
+    struct TransferParameter {
+        bytes32 previousTransferId;
+        address provider;
+        address sourceToken;
+        address targetToken;
+        bytes32 lastBlockHash;
+        uint112 amount;
+        uint64 timestamp;
+        address receiver;
+    }
+
+    function slash(
+        bytes32 lastRefundTransferId,
+        bytes32 transferId,
+        address provider,
+        address sourceToken,
+        address slasher
+    ) external;
+
+    function withdrawMargin(
+        bytes32 lastRefundTransferId,
+        bytes32 lastTransferId,
+        address provider,
+        address sourceToken,
+        uint112 amount
+    ) external;
+}

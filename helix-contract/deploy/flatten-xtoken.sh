320d11e27c78405ceb05522a2324b281f923680e
xiaoch05 <xiaoch2010@gmail.com>
2023-12-26 13:33:46 +0800

    add guard

--- helix-contract/deploy/flatten-xtoken.sh
+++ helix-contract/deploy/flatten-xtoken.sh
@@ -3,5 +3,5 @@ mkdir -p $path
 yarn flat contracts/mapping-token/v3/base/xTokenBacking.sol --output $path/xTokenBacking.sol
 yarn flat contracts/mapping-token/v3/base/xTokenIssuing.sol --output $path/xTokenIssuing.sol
 yarn flat contracts/mapping-token/v3/base/xTokenErc20.sol --output $path/xTokenErc20.sol
-yarn flat contracts/mapping-token/v2/Guard.sol --output $path/Guard.sol
+yarn flat contracts/mapping-token/v3/GuardV3.sol --output $path/GuardV3.sol
 yarn flat contracts/messagers/MsglineMessager.sol --output $path/MsglineMessager.sol

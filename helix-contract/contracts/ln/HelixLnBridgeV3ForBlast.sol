e0b17204b89dcc2a391ec66a5cfc97cf0a9d4fa8
xiaoch05 <xiaoch2010@gmail.com>
2024-03-05 15:57:10 +0800

    inherite LnBridgeV3

--- helix-contract/contracts/ln/HelixLnBridgeV3ForBlast.sol
+++ helix-contract/contracts/ln/HelixLnBridgeV3ForBlast.sol
@@ -1,54 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "@zeppelin-solidity/contracts/proxy/utils/Initializable.sol";
-import "./base/LnBridgeSourceV3.sol";
-import "./base/LnBridgeTargetV3.sol";
-import "../interfaces/IMessager.sol";
+import "./HelixLnBridgeV3.sol";
 import "../interfaces/IBlast.sol";
 import "../interfaces/IBlastPoints.sol";
 
-contract HelixLnBridgeV3ForBlast is Initializable, LnBridgeSourceV3, LnBridgeTargetV3 {
-    struct MessagerService {
-        address sendService;
-        address receiveService;
-    }
-
-    // remoteChainId => messager
-    mapping(uint256=>MessagerService) public messagers;
-
-    receive() external payable {}
-
-    function initialize(address _dao, address _blast, address _blastPoints) public initializer {
+// when register some token that support yield, don't forget to configure claimable yield
+contract HelixLnBridgeV3ForBlast is HelixLnBridgeV3 {
+    function initialize(address _dao, bytes calldata _data) public override initializer {
         _initialize(_dao);
+        (address _blast, address _blastPoints) = abi.decode(_data, (address, address));
         IBlast blast = IBlast(_blast);
         blast.configureClaimableGas();
-        blast.configureAutomaticYield();
+        blast.configureClaimableYield();
         blast.configureGovernor(_dao);
         IBlastPoints blastPoints = IBlastPoints(_blastPoints);
         blastPoints.configurePointsOperator(_dao);
-    }
-
-    // the remote endpoint is unique, if we want multi-path to remote endpoint, then the messager should support multi-path
-    function setSendService(uint256 _remoteChainId, address _remoteBridge, address _service) external onlyDao {
-        messagers[_remoteChainId].sendService = _service;
-        ILowLevelMessageSender(_service).registerRemoteReceiver(_remoteChainId, _remoteBridge);
-    }
-
-    function setReceiveService(uint256 _remoteChainId, address _remoteBridge, address _service) external onlyDao {
-        messagers[_remoteChainId].receiveService = _service;
-        ILowLevelMessageReceiver(_service).registerRemoteSender(_remoteChainId, _remoteBridge);
-    }
-
-    function _sendMessageToSource(uint256 _remoteChainId, bytes memory _payload, uint256 feePrepaid, bytes memory _extParams) whenNotPaused internal override {
-        address sendService = messagers[_remoteChainId].sendService;
-        require(sendService != address(0), "invalid messager");
-        ILowLevelMessageSender(sendService).sendMessage{value: feePrepaid}(_remoteChainId, _payload, _extParams);
-    }
-
-    function _verifyRemote(uint256 _remoteChainId) whenNotPaused internal view override {
-        address receiveService = messagers[_remoteChainId].receiveService;
-        require(receiveService == msg.sender, "invalid messager");
     }
 }
 
